d406ddf20b158cd68f10c563bcf9c0e1de12b74c
Jeroen Savat <jeroen.savat@gmail.com>
2018-12-03 21:29:27 +0100

    attempt at part2

--- 2018/typescript/day-01/main2.ts
+++ 2018/typescript/day-01/main2.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from "fs";
 const arrInput = readFileSync(`${__dirname}/input.txt`, "utf8");
-import { sum } from "../utils/index";
 
 const cleanInput = (input: string) => input.replace("\r", "");
 const arrNumbers = arrInput
@@ -8,7 +7,37 @@ const arrNumbers = arrInput
   .map(cleanInput)
   .map(parseFloat);
 
-const main = (input: number[]): number => input.reduce(sum);
+const main = (input: number[]): number => {
+  let foundDuplicate = 0;
+  const lookUp: number[] = [];
+
+  // tslint:disable
+  [...input].reduce(
+    (
+      accumulator: number,
+      currentValue: number,
+      currentIndex: number,
+      loopedArray: number[],
+    ): number => {
+      const nextResult = accumulator + currentValue;
+
+      const lookupResult:number|undefined = lookUp.find(
+        (loopedResult) => (loopedResult === nextResult)
+      );
+
+      if (lookupResult !== undefined) {
+        foundDuplicate = lookupResult;
+        console.log(`result found at ${currentIndex}`);
+        loopedArray.splice(1);
+        return nextResult;
+      }
+      lookUp.push(nextResult);
+      return nextResult;
+    },
+  );
+
+  return foundDuplicate;
+};
 
 const result = main(arrNumbers);
 console.log(result);
